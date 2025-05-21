305743d98207820d2223cb0411800f98edef879e
Cameron <cambboyle@gmail.com>
2025-05-21 15:42:25 +0100

    chore: update title to welcome message

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import { startTimer } from './components/timer'
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app')
 
-  app.innerHTML = `<h1>Kairo: Productivity App</h1>
+  app.innerHTML = `<h1>Welcome to <strong>Kairo</strong>.</h1>
           <div id=timer></div>`
 
   startTimer(document.getElementById('timer'))
